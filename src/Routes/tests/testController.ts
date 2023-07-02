627d4d72094271b69ab487d05a2eec3b480d7378
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-07-02 03:45:26 +0100

    Update testController.ts
    add comments for test route

--- src/Routes/tests/testController.ts
+++ src/Routes/tests/testController.ts
@@ -1,5 +1,9 @@
+// ==============================|| based test server running route ||============================== //
+
+// ======|| import route request types from Express
 import { Request, RequestHandler, Response } from 'express';
 
+// ======|| get test route from server running
 export const getTestData: RequestHandler = (_req: Request, res: Response) => {
-  return res.send('Express + TypeScript Serving');
+  return res.status(200).send('Express + TypeScript Serving');
 };
