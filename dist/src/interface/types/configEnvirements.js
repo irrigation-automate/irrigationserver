7ee7089063ce04db53e3901bde6f399a9c58f203
RaedRdhaounia <raedrdhaounia@gmail.com>
2025-10-01 01:43:40 +0100

    chore: remove dist directory from git tracking
    - Removed dist/ from Git's index while keeping local files
    - Ensured .gitignore properly excludes build artifacts

--- dist/src/interface/types/configEnvirements.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// ==============================|| envirement variables collections types ||============================== //
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=configEnvirements.js.map
